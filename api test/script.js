2488983eed693e7dafe4d6324cf83427a4ffe1d1
Ezhko <ucsd.camp.21@gmail.com>
2021-10-05 16:34:18 -0700

    get token; search dogs

--- api test/script.js
+++ api test/script.js
@@ -1,98 +1,76 @@
-var cityCall = "https://api.openweathermap.org/data/2.5/weather"
+var dogCall = "https://api.petfinder.com/v2/animals?type=dog&limit=5";
 
-var oneCall = 'https://api.openweathermap.org/data/2.5/onecall';
+var tokenCall = "https://api.petfinder.com/v2/oauth2/token";
 
-var apiKey = "112bf445259e35c8b97e82d67527af29";
+var apiKey = "WY3pauZYdH3LtBAMrg6nUuprjR3ob4F4QkCualrLU8uNjQuiz2";
 
-var lat = 32.7;
-var lon = -117.2;
+var apiSecret = "rUMx8wxwlkTMLB4TjZgjHsJdtK3v9Wo4P5t21lfc";
 
-var cityInput = "San Diego";
+var area = "San Diego";
+
+var breed = "Maltese";
 
 var responseText = document.getElementById('response-text');
 
-function getCityApi(url) {
-    fetch(url)
-      .then(function (response) {
-        console.log(response);
-        // display the status
-  
-        responseText.textContent = response.status;
-        // check the response status for success
-        if (response.status === 200) {
-          responseText.style.color = 'green';
-        } else {
-          responseText.style.color = 'red';
-        }
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
-  }
+// get token function
 
-getCityApi(cityCall + "?q=" + cityInput + "&appid=" + apiKey);
+function getToken (url) {
+
+fetch(url, {
+	method: 'POST',
+	body: 'grant_type=client_credentials&client_id=' + apiKey + '&client_secret=' + apiSecret,
+	headers: {
+		'Content-Type': 'application/x-www-form-urlencoded'
+	}
+}).then(function (resp) {
+
+	// Return the response as JSON
+	return resp.json();
+
+}).then(function (data) {
+
+	// Log the API data
+	console.log('token', data);
+
+  token = data.access_token;
+
+  console.log(token);
+
+  //pass token to dog search
+
+  getDogs(dogCall, token);
 
 
+});
 
-function getOneApi(url) {
-    fetch(url)
-      .then(function (response) {
-        console.log(response);
-        // display the status
-  
-        responseText.textContent = response.status;
-        // check the response status for success
-        if (response.status === 200) {
-          responseText.style.color = 'green';
-        } else {
-          responseText.style.color = 'red';
-        }
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
-  }
-  
-  getOneApi(oneCall + "?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&appid=" + apiKey);
-
-  
-var tableBody = document.getElementById("repo-table");
-var fetchButton = document.getElementById("fetch-button");
-var cityCall = "https://api.openweathermap.org/data/2.5/weather" + "?q=" + cityInput + "&appid=" + apiKey;
-
-function getCoord() {
-  // fetch request gets a list of all the repos for the node.js organization
-  
-  fetch(cityCall)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      
-      var lon = data.coord.lon;
-
-      console.log(lon);
-
-      var lat = data.coord.lat;
-
-      console.log(lat);
-
-      // Creating elements, tablerow, tabledata, and anchor
-      var tableRow = document.createElement("tr");
-      var tableData = document.createElement("td");
-      var para = document.createElement("p")
-      
-      //Appending data to table cells
-    para.innerHTML = cityInput + ": <hr> " + "lon: " + lon + ", " + "lat: " + lat + ".";
-
-      tableData.appendChild(para);
-      tableRow.appendChild(tableData);
-      tableBody.appendChild(tableRow);
-
-    });
 }
 
-fetchButton.addEventListener("click", getCoord);
+// dog search function
+
+function getDogs(url, token) {
+    fetch(url, { headers: {"Authorization": "Bearer " + token
+    }
+  }
+  )
+      .then(function (response) {
+        console.log(response);
+        // display the status
+  
+        responseText.textContent = response.status;
+        // check the response status for success
+        if (response.status === 200) {
+          responseText.style.color = 'green';
+        } else {
+          responseText.style.color = 'red';
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  }
+
+// get token first
+
+getToken(tokenCall);
+
