9de6442db05511da38d83b4a3c1b13e9ca6a713e
Ezhko <ucsd.camp.21@gmail.com>
2021-10-05 17:12:28 -0700

    add results variables

--- api test/script.js
+++ api test/script.js
@@ -2,6 +2,8 @@ var dogCall = "https://api.petfinder.com/v2/animals?type=dog&limit=5";
 
 var tokenCall = "https://api.petfinder.com/v2/oauth2/token";
 
+var orgCall = "https://api.petfinder.com/v2/organizations";
+
 var apiKey = "WY3pauZYdH3LtBAMrg6nUuprjR3ob4F4QkCualrLU8uNjQuiz2";
 
 var apiSecret = "rUMx8wxwlkTMLB4TjZgjHsJdtK3v9Wo4P5t21lfc";
@@ -40,6 +42,34 @@ fetch(url, {
 
 }
 
+function getOrg(url, token) {
+  fetch(url, { headers: {"Authorization": "Bearer " + token
+  }
+}
+)
+    .then(function (response) {
+      console.log(response);
+      // display the status
+
+      responseText.textContent = response.status;
+      // check the response status for success
+      if (response.status === 200) {
+        responseText.style.color = 'green';
+      } else {
+        responseText.style.color = 'red';
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      orgName = data.organization.name
+
+      console.log(orgName);
+
+    });
+}
+
 // dog search function
 
 function getDogs(url, token) {
@@ -62,6 +92,25 @@ function getDogs(url, token) {
       })
       .then(function (data) {
         console.log(data);
+
+        // do the following for each of the five results:
+
+        address = data.animals[0].contact.address;
+
+        console.log(address);
+
+        age = data.animals[0].age;
+
+        console.log(age);
+
+        dogName = data.animals[0].name;
+
+        console.log(dogName);
+
+        org_id = data.animals[0].organization_id;
+
+        getOrg(orgCall + "/" + org_id, token);
+
       });
   }
 
