f9bbe7169e42384e7aa2665ddbedaeae8454d5f0
Ezhko <ucsd.camp.21@gmail.com>
2021-10-06 21:18:43 -0700

    cleanup code

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,14 +167,23 @@ function getDogs(url, token) {
 
       // save data in local storage
 
-    saveData = [ [dogName_0, smallPic_0, breed_0, age_0, orgName_0], [dogName_1, smallPic_1, breed_1, age_1, orgName_1], [dogName_2, smallPic_2, breed_2, age_2, orgName_2], [dogName_3, smallPic_3, breed_3, age_3, orgName_3], [dogName_4, smallPic_3, breed_4, age_4, orgName_4]];
-    localStorage.setItem("data", saveData);
+      saveData = [
+        [dogName_0, smallPic_0, breed_0, age_0, orgName_0],
+        [dogName_1, smallPic_1, breed_1, age_1, orgName_1],
+        [dogName_2, smallPic_2, breed_2, age_2, orgName_2],
+        [dogName_3, smallPic_3, breed_3, age_3, orgName_3],
+        [dogName_4, smallPic_3, breed_4, age_4, orgName_4],
+      ];
+      localStorage.setItem("data", saveData);
 
-    saveAddress = [stringAddress_0, stringAddress_1, stringAddress_2, stringAddress_3, stringAddress_4];
-    localStorage.setItem("locations", saveAddress);
-
-    
-
+      saveAddress = [
+        stringAddress_0,
+        stringAddress_1,
+        stringAddress_2,
+        stringAddress_3,
+        stringAddress_4,
+      ];
+      localStorage.setItem("locations", saveAddress);
     });
 }
 
@@ -190,7 +199,7 @@ function initMap() {
     zoom: 10,
     center: latlng,
   };
-  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  map = new google.maps.Map(document.getElementById("#map"), mapOptions);
   codeAddress(address);
 }
 function codeAddress(puppyAddress) {
