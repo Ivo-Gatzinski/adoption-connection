857177a17d1664c52e37eacd113c5d251a70d229
Ezhko <ucsd.camp.21@gmail.com>
2021-10-06 20:46:21 -0700

    cleanup code

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,229 +10,207 @@ var apiSecret = "rUMx8wxwlkTMLB4TjZgjHsJdtK3v9Wo4P5t21lfc";
 
 // get token function
 
-function getToken (url, breed, zipCode) {
+function getToken(url, breed, zipCode) {
+  fetch(url, {
+    method: "POST",
+    body:
+      "grant_type=client_credentials&client_id=" +
+      apiKey +
+      "&client_secret=" +
+      apiSecret,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  })
+    .then(function (resp) {
+      // Return the response as JSON
+      return resp.json();
+    })
+    .then(function (data) {
+      token = data.access_token;
 
-fetch(url, {
-	method: 'POST',
-	body: 'grant_type=client_credentials&client_id=' + apiKey + '&client_secret=' + apiSecret,
-	headers: {
-		'Content-Type': 'application/x-www-form-urlencoded'
-	}
-}).then(function (resp) {
+      //pass token to dog search
 
-	// Return the response as JSON
-	return resp.json();
-
-}).then(function (data) {
-
-  token = data.access_token;
-
-  //pass token to dog search
-
-  getDogs(dogCall + "&breed=" + breed + "&location=" + zipCode + "&distance=50", token);
-
-
-});
-
+      getDogs(
+        dogCall + "&breed=" + breed + "&location=" + zipCode + "&distance=50",
+        token
+      );
+    });
 }
 
 //get organization name function
 
 function getOrg(url, token) {
-  fetch(url, { headers: {"Authorization": "Bearer " + token
-  }
-}
-)
+  fetch(url, { headers: { Authorization: "Bearer " + token } })
     .then(function (response) {
       console.log(response);
-      
+
       return response.json();
     })
     .then(function (data) {
-
       orgName = data.organization.name;
-
     });
 }
 
 // dog search function
 
 function getDogs(url, token) {
-    fetch(url, { headers: {"Authorization": "Bearer " + token
-    }
-  }
-  )
-      .then(function (response) {
-        console.log(response);
-        
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
+  fetch(url, { headers: { Authorization: "Bearer " + token } })
+    .then(function (response) {
+      console.log(response);
 
-        // do the following for each of the five results:
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
 
-        address_0 = data.animals[0].contact.address;
-        address1_0 = data.animals[0].contact.address.address1;
-        address2_0 = data.animals[0].contact.address.address2;
-        city_0 = data.animals[0].contact.address.city;
-        state_0 = data.animals[0].contact.address.state;
-        postcode_0 = data.animals[0].contact.address.postcode;
-        stringAddress_0 = address1_0 + ", " + city_0 + ", " + state_0 + ", " + postcode_0;
-        
+      // do the following for each of the five results:
 
-        address_1 = data.animals[1].contact.address;
-        address1_1 = data.animals[1].contact.address.address1;
-        address2_1 = data.animals[1].contact.address.address2;
-        city_1 = data.animals[1].contact.address.city;
-        state_1 = data.animals[1].contact.address.state;
-        postcode_1 = data.animals[1].contact.address.postcode;
-        stringAddress_1 = address1_1 + ", " + city_1 + ", " + state_1 + ", " + postcode_1;
-        
+      //address
 
-        address_2 = data.animals[2].contact.address;
-        address1_2 = data.animals[2].contact.address.address1;
-        address2_2 = data.animals[2].contact.address.address2;
-        city_2 = data.animals[2].contact.address.city;
-        state_2 = data.animals[2].contact.address.state;
-        postcode_2 = data.animals[2].contact.address.postcode;
-        stringAddress_2 = address1_2 + ", " + city_2 + ", " + state_2 + ", " + postcode_2;
-        
+      address_0 = data.animals[0].contact.address;
+      address1_0 = data.animals[0].contact.address.address1;
+      address2_0 = data.animals[0].contact.address.address2;
+      city_0 = data.animals[0].contact.address.city;
+      state_0 = data.animals[0].contact.address.state;
+      postcode_0 = data.animals[0].contact.address.postcode;
+      stringAddress_0 =
+        address1_0 + ", " + city_0 + ", " + state_0 + ", " + postcode_0;
 
-        address_3 = data.animals[3].contact.address;
-        address1_3 = data.animals[3].contact.address.address1;
-        address2_3 = data.animals[3].contact.address.address2;
-        city_3 = data.animals[3].contact.address.city;
-        state_3 = data.animals[3].contact.address.state;
-        postcode_3 = data.animals[3].contact.address.postcode;
-        stringAddress_3 = address1_3 + ", " + city_3 + ", " + state_3 + ", " + postcode_3;
-        
+      address_1 = data.animals[1].contact.address;
+      address1_1 = data.animals[1].contact.address.address1;
+      address2_1 = data.animals[1].contact.address.address2;
+      city_1 = data.animals[1].contact.address.city;
+      state_1 = data.animals[1].contact.address.state;
+      postcode_1 = data.animals[1].contact.address.postcode;
+      stringAddress_1 =
+        address1_1 + ", " + city_1 + ", " + state_1 + ", " + postcode_1;
 
-        address_4 = data.animals[4].contact.address;
-        address1_4 = data.animals[4].contact.address.address1;
-        address2_4 = data.animals[4].contact.address.address2;
-        city_4 = data.animals[4].contact.address.city;
-        state_4 = data.animals[4].contact.address.state;
-        postcode_4 = data.animals[4].contact.address.postcode;
-        stringAddress_4 = address1_4 + ", " + city_4 + ", " + state_4 + ", " + postcode_4;
-      
-// Age
-        age_0 = data.animals[0].age;
-        age_1 = data.animals[1].age;
-        age_2 = data.animals[2].age;
-        age_3 = data.animals[3].age;
-        age_4 = data.animals[4].age;
+      address_2 = data.animals[2].contact.address;
+      address1_2 = data.animals[2].contact.address.address1;
+      address2_2 = data.animals[2].contact.address.address2;
+      city_2 = data.animals[2].contact.address.city;
+      state_2 = data.animals[2].contact.address.state;
+      postcode_2 = data.animals[2].contact.address.postcode;
+      stringAddress_2 =
+        address1_2 + ", " + city_2 + ", " + state_2 + ", " + postcode_2;
 
-        // Name
+      address_3 = data.animals[3].contact.address;
+      address1_3 = data.animals[3].contact.address.address1;
+      address2_3 = data.animals[3].contact.address.address2;
+      city_3 = data.animals[3].contact.address.city;
+      state_3 = data.animals[3].contact.address.state;
+      postcode_3 = data.animals[3].contact.address.postcode;
+      stringAddress_3 =
+        address1_3 + ", " + city_3 + ", " + state_3 + ", " + postcode_3;
 
-        dogName_0 = data.animals[0].name;
-        dogName_1 = data.animals[1].name;
-        dogName_2 = data.animals[2].name;
-        dogName_3 = data.animals[3].name;
-        dogName_4 = data.animals[4].name;
+      address_4 = data.animals[4].contact.address;
+      address1_4 = data.animals[4].contact.address.address1;
+      address2_4 = data.animals[4].contact.address.address2;
+      city_4 = data.animals[4].contact.address.city;
+      state_4 = data.animals[4].contact.address.state;
+      postcode_4 = data.animals[4].contact.address.postcode;
+      stringAddress_4 =
+        address1_4 + ", " + city_4 + ", " + state_4 + ", " + postcode_4;
 
-        //Pic
+      // Age
 
-        smallPic_0 = data.animals[0].photos[0].small;
-        smallPic_1 = data.animals[1].photos[1].small;
-        smallPic_2 = data.animals[2].photos[2].small;
-        smallPic_3 = data.animals[3].photos[3].small;
-        // smallPic_4 = data.animals[4].photos[4].small;
+      age_0 = data.animals[0].age;
+      age_1 = data.animals[1].age;
+      age_2 = data.animals[2].age;
+      age_3 = data.animals[3].age;
+      age_4 = data.animals[4].age;
 
-        //Breed 
+      // Name
 
-        breed_0 = data.animals[0].breeds.primary;
-        breed_1 = data.animals[1].breeds.primary;
-        breed_2 = data.animals[2].breeds.primary;
-        breed_3 = data.animals[3].breeds.primary;
-        breed_4 = data.animals[4].breeds.primary;
+      dogName_0 = data.animals[0].name;
+      dogName_1 = data.animals[1].name;
+      dogName_2 = data.animals[2].name;
+      dogName_3 = data.animals[3].name;
+      dogName_4 = data.animals[4].name;
 
-// Org Name
+      //Pic
 
-        org_id_0 = data.animals[0].organization_id;
-        org_id_1 = data.animals[1].organization_id;
-        org_id_2 = data.animals[2].organization_id;
-        org_id_3 = data.animals[3].organization_id;
-        org_id_4 = data.animals[4].organization_id;
+      smallPic_0 = data.animals[0].photos[0].small;
+      smallPic_1 = data.animals[1].photos[1].small;
+      smallPic_2 = data.animals[2].photos[2].small;
+      smallPic_3 = data.animals[3].photos[3].small;
+      // WEIRD MYSTERY ERROR: smallPic_4 = data.animals[4].photos[4].small;
 
-       orgName_0 = getOrg(orgCall + "/" + org_id_0, token);
-       orgName_1 = getOrg(orgCall + "/" + org_id_1, token);
-       orgName_2 = getOrg(orgCall + "/" + org_id_2, token);
-       orgName_3 = getOrg(orgCall + "/" + org_id_3, token);
-       orgName_4 = getOrg(orgCall + "/" + org_id_4, token);
+      //Breed
 
-        codeAddress(stringAddress_0);
-        codeAddress(stringAddress_1);
-        codeAddress(stringAddress_2);
-        codeAddress(stringAddress_3);
-        codeAddress(stringAddress_4);
+      breed_0 = data.animals[0].breeds.primary;
+      breed_1 = data.animals[1].breeds.primary;
+      breed_2 = data.animals[2].breeds.primary;
+      breed_3 = data.animals[3].breeds.primary;
+      breed_4 = data.animals[4].breeds.primary;
 
-        // save data in local storage
+      // Org Name
 
+      org_id_0 = data.animals[0].organization_id;
+      org_id_1 = data.animals[1].organization_id;
+      org_id_2 = data.animals[2].organization_id;
+      org_id_3 = data.animals[3].organization_id;
+      org_id_4 = data.animals[4].organization_id;
 
+      orgName_0 = getOrg(orgCall + "/" + org_id_0, token);
+      orgName_1 = getOrg(orgCall + "/" + org_id_1, token);
+      orgName_2 = getOrg(orgCall + "/" + org_id_2, token);
+      orgName_3 = getOrg(orgCall + "/" + org_id_3, token);
+      orgName_4 = getOrg(orgCall + "/" + org_id_4, token);
 
-      });
-  }
+      codeAddress(stringAddress_0);
+      codeAddress(stringAddress_1);
+      codeAddress(stringAddress_2);
+      codeAddress(stringAddress_3);
+      codeAddress(stringAddress_4);
 
+      // save data in local storage
+    });
+}
 
 // google api
 
 var map;
 
-  function initMap() {
-    var address = "931 Torrey Pines drive Paso Robles, Ca. 93346"
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var mapOptions = {
-      zoom: 10,
-      center: latlng
-    }
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    codeAddress(address);
+function initMap() {
+  var address = "931 Torrey Pines drive Paso Robles, Ca. 93346";
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+    zoom: 10,
+    center: latlng,
+  };
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  codeAddress(address);
 }
-  function codeAddress(puppyAddress) {
-    geocoder.geocode( { 'address': puppyAddress}, function(results, status) {
-      if (status == 'OK') {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            title: "click to zoom",
-            position: results[0].geometry.location
-        });
-        marker.addListener("click", () => {
-          map.setZoom(8);
-          map.setCenter(marker.getPosition());
+function codeAddress(puppyAddress) {
+  geocoder.geocode({ address: puppyAddress }, function (results, status) {
+    if (status == "OK") {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        title: "click to zoom",
+        position: results[0].geometry.location,
       });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-  }
+      marker.addListener("click", () => {
+        map.setZoom(8);
+        map.setCenter(marker.getPosition());
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
 
 // get parameters, get token, then submit parameters to get token and then search
 
 $("#form").on("submit", function (event) {
+  event.preventDefault();
 
-event.preventDefault();
+  zipCode = $("#code").val();
 
-zipCode = $("#code").val();
+  breed = $("#breed").val();
 
-breed = $("#breed").val();
-
-console.log(zipCode);
-console.log(breed);
-
-getToken(tokenCall, breed, zipCode);
-
-
-}
-);
-
-
-  
-
-  
-
-
-
-
+  getToken(tokenCall, breed, zipCode);
+});
